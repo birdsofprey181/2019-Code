1387c2f17eb0512275f47ee43eda18895607a5aa
Gwen Miller <gwenmillerpathways@gmail.com>
2019-01-16 12:49:20 -0500

    Added shifting code

--- src/main/java/frc/robot/PneumaticsTest.java
+++ src/main/java/frc/robot/PneumaticsTest.java
@@ -2,9 +2,11 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 public class PneumaticsTest {
-    static DoubleSolenoid gripperSol = new DoubleSolenoid(0, 2, 3);
+		static DoubleSolenoid gripperSol = new DoubleSolenoid(0, 2, 3);
+		static DoubleSolenoid shifterSol = new DoubleSolenoid(0, 0, 1);
     static Joystick driveStick = new Joystick(0);
     static Joystick opStick = new Joystick(1);
 
@@ -15,13 +17,27 @@ public class PneumaticsTest {
 		gripperSol.set(DoubleSolenoid.Value.kForward);	//moves solenoid backwards
     }
     public static void grip() {
-		if(driveStick.getRawButton(2) == true || opStick.getRawButton(2) == true) {	//if the "2" button pressed on the driver stick, run the program
-			startGrip();	//runs the "unGrip" program
-			System.out.println("Letting Go");
+			if(driveStick.getRawButton(2) == true || opStick.getRawButton(2) == true) {	//if the "2" button pressed on the driver stick, run the program
+				startGrip();	//runs the "unGrip" program
+				System.out.println("Letting Go");
+			}
+			else if(driveStick.getRawButton(2) == false && opStick.getRawButton(2) == false) {	//if the "2" button released on the driver stick, run the program
+				unGrip();	//runs the "startGrip" program
+				System.out.println("Gripping");
+			}
 		}
-		else if(driveStick.getRawButton(2) == false && opStick.getRawButton(2) == false) {	//if the "2" button released on the driver stick, run the program
-			unGrip();	//runs the "startGrip" program
-			System.out.println("Gripping");
+		public static void highGear() {
+			shifterSol.set(DoubleSolenoid.Value.kForward);
 		}
-	}
+		public static void lowGear() {
+			shifterSol.set(DoubleSolenoid.Value.kForward);
+		}
+		public static void shiftGears() {
+			if(driveStick.getRawButton(1) == true){
+				highGear();
+			}
+			else{
+				lowGear();
+			}
+		}
 }
