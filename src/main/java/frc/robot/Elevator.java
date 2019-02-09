49b71ea53f16ed1c62cb15ba139193d2e96466c7
Gwen Miller <gwenmillerpathways@gmail.com>
2019-02-09 11:24:23 -0500

    added encoders

--- src/main/java/frc/robot/Elevator.java
+++ src/main/java/frc/robot/Elevator.java
@@ -3,10 +3,21 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Encoder;
 
 public class Elevator {
 
     static Spark elevator = new Spark(2);
+    static Encoder elevEncoder = new Encoder(0, 1, true, Encoder.EncodingType.k4X);
+
+    public static void resetElevEncoder(){
+        elevEncoder.reset();
+    }
+
+    public static void elevEncoderTest(){
+        double elevDistance = elevEncoder.getDistance();
+        
+    }
 
     public static void elevControl(Joystick opStick) {
         elevator.set(opStick.getY());
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -34,6 +34,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    Elevator.resetElevEncoder();
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
