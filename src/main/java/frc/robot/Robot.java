296ffa91d711f83d528b8a382becfad941bf1d47
Gwen Miller <gwenmillerpathways@gmail.com>
2019-02-16 12:23:11 -0500

    commented out testing code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -103,7 +103,7 @@ public class Robot extends TimedRobot {
     Elevator.elevControl(opStick.getY());
     Drivetrain.drive(driveStick.getY(), driveStick.getZ());
     Elevator.elevEncoderTest(opStick);
-    Elevator.elevBrake();
+    // Elevator.elevBrake();
     Elevator.encoderTest();
   }
 
