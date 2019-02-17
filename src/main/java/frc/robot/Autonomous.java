385acde0ff83b7eec28dfc05e5b9eab9d5c8c268
Gwen Miller <gwenmillerpathways@gmail.com>
2019-02-17 12:43:49 -0500

    added 'auto' code ;)

---
+++ src/main/java/frc/robot/Autonomous.java
@@ -0,0 +1,30 @@
+//Gwen Miller 02/17/19
+
+package frc.robot;
+
+public class Autonomous {
+    robot.doStuff()
+    if(notWinning == true){
+        robot.selfDestruct();
+    }else if(notWinning == false){
+        robot.victoryRoyale();
+        robot.defaultDance();
+    }
+    /*
+    if going for ship
+        raise to ship
+        drive to ship
+        push hatch off
+    else if going for rocket
+        if level 1
+            raise to level 1
+        if level 2
+            raise to level 2
+        if level 3
+            raise to level 3
+        drive to rocket
+        push hatch off
+    else if not going for anything
+        drive forward
+    */
+}
