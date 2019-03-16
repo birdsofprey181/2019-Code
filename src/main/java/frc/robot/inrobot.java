871cc87bedb62fe2f62d312c359597771bf01dd4
Jay8002 <36057671+Jay8002@users.noreply.github.com>
2019-03-16 12:26:27 -0400

    Added intorobot file and I made an if and else statement to be on or off

---
+++ src/main/java/frc/robot/inrobot.java
@@ -0,0 +1,23 @@
+package frc.robot;
+//done by Evan Belcourt in 3/16/19
+//pulic class for turning vision proccessing on and off 
+public class inrobot
+{
+    //creates light status boolean variable
+    public static Boolean lightStatus = new Boolean(true);
+
+    //boolean for turning the vison on and off for the limelight
+    public static void main(String [] args){
+        //ligth status starts as false and turns off
+        if (lightStatus = false){
+           int lightStatus;
+           lightStatus = 1;
+        }
+        //light status turns to true and turns on vision proccesing
+        if  (lightStatus = true){
+            int lightStatus;
+            lightStatus = 0;
+
+        }
+    }
+}
